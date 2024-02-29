6aa8781b6495d6ef06548ce37cd31319f9ec4b90
danieldemac <12dancabral@gmail.com>
2024-02-29 18:16:59 -0300

    Finalizando primeira parte

--- frontend/app/src/components/Login.js
+++ frontend/app/src/components/Login.js
@@ -5,6 +5,8 @@ function Login(){
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [user, setUser] = useState(null);
 
 
 
@@ -12,7 +14,8 @@ function Login(){
         e.preventDefault();
 
         console.log(email, password);
-
+        
+      try{
         const response = await axios.post('http://localhost:3000/login',
             JSON.stringify({email, password}),
             {
@@ -20,11 +23,30 @@ function Login(){
             }
 
         );
+        console.log(response.data);
+        setUser(response.data);
+      }catch(error){
+        if(!error?.response){
+          setError('Erro ao acessar o servidor');
+        } else if (error.response.status == 401){
+            setError("Usuário ou senha inválidos");
+        }
+      }
     }
+
+    const handleLogout = async (e) => {
+
+      e.preventDefault();
+      setUser(null);
+      setError('');
+        
+    };
 
 
     return (
       <div className='login-form-wrap'>
+        {user == null ? (
+          <div>
       <img className="avatar_base" src="datametrica.jpg" alt="avatar_base" />
       <h2>Painel Pesquisadores</h2>
       <form className='login-form'>
@@ -48,6 +70,21 @@ function Login(){
           onClick={(e) => handleLogin(e)}>
             Login</button>
       </form>
+      <p className="errocancelar">{error}</p>
+      </div>
+      ):(
+        <div>
+          <img className="avatar" src="avatar.png" alt="avatar" />
+          <p className='bem_vindo'>Você logou no painel pesquisadores, {user.name}.</p>
+          <p className="bem_vindo">Bem vindo!</p>
+          <button 
+          type='button' 
+          className='btn-login'
+          onClick={(e) => handleLogout(e)}>
+            Sair</button>
+        </div>
+      )
+      }
     </div>
     );
   
